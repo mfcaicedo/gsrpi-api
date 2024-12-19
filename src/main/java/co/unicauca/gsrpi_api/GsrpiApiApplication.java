0b4195133b054363dff35ea132562bd7447f151a
mfcaicedo <mfcaicedo@unicauca.edu.co>
2024-12-19 15:05:41 -0500

    feat se inicializa la api de gsrpi

---
+++ src/main/java/co/unicauca/gsrpi_api/GsrpiApiApplication.java
@@ -0,0 +1,13 @@
+package co.unicauca.gsrpi_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsrpiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsrpiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/unicauca/gsrpi_api/GsrpiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unicauca.gsrpi_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsrpiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
