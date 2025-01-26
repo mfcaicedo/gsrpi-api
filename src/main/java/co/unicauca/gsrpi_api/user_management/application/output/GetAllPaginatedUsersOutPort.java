ac9e0df085f997a4b90129e799f71e83701fe653
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-01-26 18:01:40 -0500

    fix: se elimina feature demo de hexagonal

--- src/main/java/co/unicauca/gsrpi_api/user_management/application/output/GetAllPaginatedUsersOutPort.java
+++
@@ -1,9 +0,0 @@
-package co.unicauca.gsrpi_api.user_management.application.output;
-
-import co.unicauca.gsrpi_api.user_management.domain.model.User;
-
-import java.util.List;
-
-public interface GetAllPaginatedUsersOutPort {
-    List<User> getAllPaginatedUsers();
-}
